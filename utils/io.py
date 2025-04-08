1074ae1d17b24cf1d80ae4c134a8c8b342ccab2e
Mark Vickers <markdvickers@icloud.com>
2025-04-08 10:41:48 -0600

    feat: finished data cleaning.

--- utils/io.py
+++ utils/io.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd
 
-q_load_data = '''SELECT ConPri.*, ConPriAssumptions.*, Project.ID, Project.offYr1, Project.offYr2, Project.offYr3,
+q_load_data = '''SELECT ConPri.*, ConPriAssumptions.*, Project.pID AS simulationName, Project.offYr1, Project.offYr2, Project.offYr3,
     Project.offYr4, Project.offYr5, Project.offYr6, Project.offYr7, Project.offYr8, Project.offYr9, 
     Project.offYr10, Project.ovRating FROM Conpri
     LEFT JOIN ConPriAssumptions ON ConPri.aChoice = ConPriAssumptions.aID
@@ -9,9 +9,9 @@ q_load_data = '''SELECT ConPri.*, ConPriAssumptions.*, Project.ID, Project.offYr
 
 def load_data(db_path: str) -> pd.DataFrame:
     conn = sqlite3.connect(db_path)
-
+    
     df = pd.read_sql_query(q_load_data, conn)
-    df = df[1:] # Might not need this
+    df = df.drop(columns=['prunID','aChoice','aID'], axis=1, errors='ignore')
 
     conn.close()
 
