b9ba970538720ee385456d725ea3eb8a7cfeff65
Mark Vickers <markdvickers@icloud.com>
2025-04-08 10:26:18 -0600

    feat: query database and load required data into df.

--- contract_pricing.py
+++ contract_pricing.py
@@ -1,8 +1,12 @@
-import sqlite3
-import pandas as pd
+from utils.io import *
+
+DB_PATH = "data/risk_model.db"
 
 def main():
-    pass
+    df_conpri = load_data(DB_PATH)
+
+    print(df_conpri)
+    print(df_conpri.info())
 
 if __name__ == "__main__":
     main()
---
+++ utils/io.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+
+q_load_data = '''SELECT ConPri.*, ConPriAssumptions.*, Project.ID, Project.offYr1, Project.offYr2, Project.offYr3,
+    Project.offYr4, Project.offYr5, Project.offYr6, Project.offYr7, Project.offYr8, Project.offYr9, 
+    Project.offYr10, Project.ovRating FROM Conpri
+    LEFT JOIN ConPriAssumptions ON ConPri.aChoice = ConPriAssumptions.aID
+    LEFT JOIN Project ON ConPri.prunID = Project.ID;'''
+
+def load_data(db_path: str) -> pd.DataFrame:
+    conn = sqlite3.connect(db_path)
+
+    df = pd.read_sql_query(q_load_data, conn)
+    df = df[1:] # Might not need this
+
+    conn.close()
+
+    return df
