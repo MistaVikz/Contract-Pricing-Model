c7f53c667b81ac7ed51c705bd3ccccf725ef675f
Mark Vickers <markdvickers@icloud.com>
2025-04-27 15:13:21 -0600

    fix: capped discount rates/irr at 100%

--- utils/calc.py
+++ utils/calc.py
@@ -18,7 +18,7 @@ def calc_top_bottom_discount_rate(df_spread, spreadChoice, ovRating, cLength, di
     """
     # Filter df_spread by spreadChoice and ovRating
     df_spread_filtered = df_spread[(df_spread['sType'] == spreadChoice) & (df_spread['rating'] == ovRating)]
-    
+
     # If not C then top/bottom spreads are different
     if ovRating != 'C':
         # Lookup the top and bottom spreads
@@ -29,7 +29,6 @@ def calc_top_bottom_discount_rate(df_spread, spreadChoice, ovRating, cLength, di
                 spread = df_spread_filtered[spread_col].iloc[0]
             else:
                 spread = df_spread_filtered[spread_col].iloc[len(df_spread_filtered) - 1]
-            return discCorpContract + (spread / 100)
         # For 8 and 9 year contracts, use the 10 and 7 year spreads respectively
         elif cLength == 8 or cLength == 9:
             spread_plus_col = 's10Yr'
@@ -43,7 +42,6 @@ def calc_top_bottom_discount_rate(df_spread, spreadChoice, ovRating, cLength, di
                 spread_plus = df_spread_filtered[spread_plus_col].iloc[len(df_spread_filtered) - 1]
                 spread_minus = df_spread_filtered[spread_minus_col].iloc[len(df_spread_filtered) - 1]
                 spread = ((spread_plus + spread_minus) / (10-7)) + spread_minus
-            return discCorpContract + (spread / 100)
         # For all other lengths, use the spread for the next and previous lengths
         else:
             spread_plus_col = f's{cLength + 1}Yr'
@@ -57,27 +55,29 @@ def calc_top_bottom_discount_rate(df_spread, spreadChoice, ovRating, cLength, di
                 spread_plus = df_spread_filtered[spread_plus_col].iloc[len(df_spread_filtered) - 1]
                 spread_minus = df_spread_filtered[spread_minus_col].iloc[len(df_spread_filtered) - 1]
                 spread = ((spread_plus + spread_minus) / (cLength + 1 - (cLength - 1))) + spread_minus
-            return discCorpContract + (spread / 100)
     else:
         # For C rating, use the same spread for top and bottom
         if cLength == 1 or cLength == 2 or cLength == 3 or cLength == 5 or cLength == 7 or cLength == 10:
             spread_col = f's{cLength}Yr'
             spread = df_spread_filtered[spread_col].iloc[0]
-            return discCorpContract + (spread / 100)
         elif cLength == 8 or cLength == 9:
             spread_plus_col = 's10Yr'
             spread_minus_col = 's7Yr'
             spread_plus = df_spread_filtered[spread_plus_col].iloc[0]
             spread_minus = df_spread_filtered[spread_minus_col].iloc[0]
             spread = ((spread_plus + spread_minus) / (10-7)) + spread_minus
-            return discCorpContract + (spread / 100)
         else:
             spread_plus_col = f's{cLength + 1}Yr'
             spread_minus_col = f's{cLength - 1}Yr'
             spread_plus = df_spread_filtered[spread_plus_col].iloc[0]
             spread_minus = df_spread_filtered[spread_minus_col].iloc[0]
             spread = ((spread_plus + spread_minus) / (cLength + 1 - (cLength - 1))) + spread_minus
-            return discCorpContract + (spread / 100)            
+
+    # Cap the discount rate at 1.0 (100%)
+    discount_rate = discCorpContract + (spread / 100)
+    if discount_rate > 1:
+        discount_rate = 1
+    return discount_rate        
 
 def calc_ra_price(discount_rate, techFuncPrice, cLength):
     """
@@ -228,6 +228,10 @@ def calc_irr(cashYr1, cashYr2, cashYr3, cashYr4, cashYr5, cashYr6, cashYr7, cash
     elif len(cashFlowsNotNull) == 1:
         return cashFlowsNotNull[0]
     irr = round(npf.irr(cashFlowsNotNull), 2)
+
+    # Cap IRR at 1.0 (100%)
+    if irr > 1:
+        irr = 1
     
     if pd.isnull(irr):
         print(f'IRR {split} could not be calculated for {simulationName}. Check cash flows.')
